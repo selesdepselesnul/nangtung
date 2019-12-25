5ea3e9af49fb76218669a997379de7e0a81c2ae1
Moch deden <moch.deden.r@gmail.com>
2019-12-25 10:51:35 +0700

    Add play-sound

--- dev.dockerfile
+++ dev.dockerfile
@@ -1,3 +1,5 @@
 FROM node:12.14.0-buster
 LABEL maintainer="moch.deden.r@gmail.com"
 RUN npm install
+RUN apt-get update; \
+    apt-get install -y mpg123;
--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Masuk bro');
+const player = require('play-sound')({player: "mpg123"});
+
+player.play('./alarm.mp3', function(err){
+    if (err) {
+        console.log(err);
+    }
+});
