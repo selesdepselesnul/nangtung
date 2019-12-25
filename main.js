1c9c42b0630497a319176291c014a84ada88869f
Moch deden <moch.deden.r@gmail.com>
2019-12-25 11:51:29 +0700

    Update mp3 file

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
-const player = require('play-sound')({player: "mpg123"});
+const player = require('play-sound')({player: "mplayer"});
 
-player.play('./alarm.mp3', function(err){
-    if (err) {
-        console.log(err);
+function isOdd(n) {
+    return Math.abs(n % 2) == 1;
+}
+
+function playAlarm(i) {
+    let mp3FileName;
+
+    if(isOdd(i)) {
+        mp3FileName = 'nangtung';
+    } else {
+        mp3FileName = 'mata';
     }
-});
+
+    console.log(`${mp3FileName} boss !`);
+
+    player.play(`${__dirname}/${mp3FileName}.mp3`, function(err){
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+function minuteToMS(minute) {
+    return minute * 60000;    
+}
+
+function main() {
+    const args = process.argv.slice(2);
+    for(let i=0;i<args.length;i++) {
+        const ms = minuteToMS(parseInt(args[i]));
+        setInterval(function() {
+            playAlarm(i+1);
+        }, ms);
+    }
+}
+
+main();
